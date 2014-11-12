321ddcbd534d1048b339ba6d94f66dade0eb11d0
njp <n.pearman@gmail.com>
2014-11-12 20:17:53 +0100

    setting the order of the areas by position along the valley

--- www/js/mock-data.js
+++ www/js/mock-data.js
@@ -3,15 +3,15 @@
 
 var mockApi = {
   areas: [
-    { id: 1, name: "Chamonix" },
-    { id: 2, name: "Servoz" },
-    { id: 3, name: "Les Houches" },
-    { id: 4, name: "Vaudagne" },
-    { id: 5, name: "Bossons/Pelerins" },
-    { id: 6, name: "Les Praz" },
-    { id: 7, name: "Les Tines" },
-    { id: 8, name: "Argentière" },
-    { id: 9, name: "Le Tour" }
+    { id:  2, name: "Servoz" },
+    { id:  4, name: "Vaudagne" },
+    { id:  3, name: "Les Houches" },
+    { id:  5, name: "Bossons/Pelerins" },
+    { id:  1, name: "Chamonix" },
+    { id:  6, name: "Les Praz" },
+    { id:  7, name: "Les Tines" },
+    { id:  8, name: "Argentière" },
+    { id:  9, name: "Le Tour" }
   ],
   searchResult: [
     [{
