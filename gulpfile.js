84d617f6744857ce6141bfdd387650ba13f661c2
Patrik Spathon <patrik@spathon.com>
2015-10-23 11:21:25 +0200

    Update files

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,9 @@ var paths = {
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/**.*')
+  gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
+    .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
